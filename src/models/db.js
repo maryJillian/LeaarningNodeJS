54b2586afb5afe746723cb5bd15ab1bec695b0d8
MariaJillian <mariajillian11@gmail.com>
2023-01-08 18:34:11 +0300

    add DB_HOST

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
+const DB_HOST = process.env.DB_HOST || 'mongodb://0.0.0.0:27017/BookDB';
 
-mongoose.connect('mongodb://0.0.0.0:27017/BookDB',
+mongoose.connect(DB_HOST,
   (error) => {
     if (!error) {
-      console.log('MongoDB Connection Succeeded.')}
-    else {
+      console.log('MongoDB Connection Succeeded.')
+    } else {
       console.log('Error in DB connection: ' + error)
     }
   });
